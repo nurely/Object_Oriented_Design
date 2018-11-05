2808ca7972dc5c5daf30ddad0ac55a38cc5fb489
kanzabatool <33962567+kanzabatool@users.noreply.github.com>
2018-11-05 23:24:02 +0300

    Update bridge_pattern.py

--- bridge_pattern.py
+++ bridge_pattern.py
@@ -1,40 +1,55 @@
 
+
 class shape():
-    def __init__(self,type):
-        self.type = type
+    def __init__(self,types):
+        self.types = types
         
     def draw(self):
         pass
 
 class circle(shape):
-    def draw(self,type):
+    def draw(self,types):
         print("Drawing the Circle...!")
 
 class rectangle(shape):
-    def draw(self, rectangle):
+    def draw(self, types):
         print("Drawing the Rectangle...!")
 
 class v1Rectangle(rectangle):
+    def __init__(self):
+        super().__init__(types = rectangle)
     def drawline(self):
         print("Method 1 drawing rectangle")
 
 class v2Rectangle(rectangle):
+    def __init__(self):
+        super().__init__(types = rectangle)
     def drawline(self):
         print("Method 2 drawing rectangle")
 
 class v1Circle(circle):
+    def __init__(self):
+        super().__init__(types = circle)
     def drawcircle(self):
         print("Method 1 drawing circle")
 
 class v2Circle(circle):
+    def __init__(self):
+        super().__init__(types = circle)
     def drawcircle(self):
         print("Method 2 drawing circle")
 
-class DP1(self,v1Circle,v1Rectangle):
+
+class DP1(v1Circle,v1Rectangle):
+    def __init__(self):
+        super().__init__(v1Circle,v1Rectangle)
     self.drawline(v1Rectangle)
     self.drawcircle(v1Circle)
 
-class DP2(v2Circle,v2Rectangle):
+class DP2(v2Circle,v2Rectangle):    
+    def __init__(self):
+        super().__init__(v2Circle,v2Rectangle)
+    
     self.drawline(v2Rectangle)
     self.drawcircle(v2Circle)
 
@@ -46,5 +61,3 @@ if __name__ == '__main__':
         DP1()
     else:
         DP2()
-
-
